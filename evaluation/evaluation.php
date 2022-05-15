af745d3ee3bdbae3acca1719f0ef70b30da48aee
sragguem <82701277+sragguem@users.noreply.github.com>
2022-05-15 04:04:11 +0000

    Add files via upload

---
+++ evaluation/evaluation.php
@@ -0,0 +1,14 @@
+
+<?php
+
+if (isset($_COOKIE['vote'])){
+    echo "Vous avez déja voté : ". strtoupper($_COOKIE['vote']).". Vous devez attendre.";
+}
+else{
+    $vote = $_POST["sondage"];
+
+    setcookie('vote', $vote, time()+60*2);
+
+    echo "Deja fait " .strtoupper($vote).".";
+
+}
